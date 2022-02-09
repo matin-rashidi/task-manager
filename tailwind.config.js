c0828f6527d2ebd168ef13a054803f0a3e943324
Matin Rashidi <matinrashidi0118@gmail.com>
2022-02-09 13:11:10 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/components/TodosList.js",
+    "./src/components/GetTodo.js",
+    "./src/components/shared/Header.js",
+    "./src/components/shared/EditModal.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

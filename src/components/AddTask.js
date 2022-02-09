9542020ca95167c14d6faebac0101dfebf9ccc8e
Matin Rashidi <matinrashidi0118@gmail.com>
2022-02-09 20:08:01 +0330

    Listbox added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,15 @@
 import React from "react";
 
+// Components
+import ListBox from "./ListBox";
+import GetTaskTitle from "./GetTaskTitle";
+
 const AddTask = () => {
   return (
-    <main className="m-5">
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="New task..."
-          className="w-full p-2 outline-none font-semibold text-teal-800 border-2 border-r-0 rounded-md focus:border-teal-800 focus:shadow-md transition-all"
-        />
-        <button className="w-14 h-full p-2 rounded-r-md bg-teal-800 text-white absolute -right-px">✚</button>
-      </div>
-    </main>
+    <section className="m-5">
+      <GetTaskTitle />
+      <ListBox />
+    </section>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: [
     "./src/components/Header.js",
-    "./src/components/AddTask.js"
+    "./src/components/AddTask.js",
+    "./src/components/GetTaskTitle.js",
+    "./src/components/ListBox.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

7c9da7ba1062e0b58c3e60faeaed5aa6da4cf19f
Matin Rashidi <matinrashidi0118@gmail.com>
2022-02-10 16:59:27 +0330

    Some styles changed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -39,7 +39,8 @@ const AddTask = () => {
         onSubmit={(event) => {
           event.preventDefault();
           task.title.trim().length > 0 && dispatch({type: "ADDTASK", payload: {...task, id: v4()}});
-          setTask({...task, title: ''})
+          setTask({...task, title: ''});
+          setIsShowList(false);
         }} 
         className="relative">
         <input
@@ -57,20 +58,27 @@ const AddTask = () => {
       </button>
 
       {isShowList && (
-        <div className="w-full right-5 bg-white flex flex-col border rounded-md border-teal-800 shadow-md">
-          <button onClick={changeCategory} name="Persional" className="p-3 border-b hover:bg-teal-50 transition-all rounded-t-md">
+        <>
+        <div onClick={() => setIsShowList(false)} className="fixed top-0 right-0 bottom-0 left-0"></div>
+        <div className="absolute w-4/12 min-w-fit right-5 bg-white flex flex-col border rounded-md shadow-md overflow-hidden">
+          <button onClick={changeCategory} name="Persional" className="p-3 hover:bg-teal-50 transition-all text-left">
+            <span className="mr-6 font-bold text-teal-800">P</span>
             Persional
           </button>
-          <button onClick={changeCategory} name="Team" className="p-3 border-b hover:bg-teal-50 transition-all">
+          <button onClick={changeCategory} name="Team" className="p-3 hover:bg-teal-50 transition-all text-left">
+            <span className="mr-6 font-bold text-teal-800">T</span>
             Team
           </button>
-          <button onClick={changeCategory} name="Homeworks" className="p-3 border-b hover:bg-teal-50 transition-all">
+          <button onClick={changeCategory} name="Homeworks" className="p-3 hover:bg-teal-50 transition-all text-left">
+            <span className="mr-6 font-bold text-teal-800">H</span>
             Homeworks
           </button>
-          <button onClick={changeCategory} name="University" className="p-3 hover:bg-teal-50 rounded-b-md">
+          <button onClick={changeCategory} name="University" className="p-3 hover:bg-teal-50 text-left">
+            <span className="mr-6 font-bold text-teal-800">U</span>
             University
           </button>
         </div>
+        </>
       )}
 
     </section>

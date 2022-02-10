e3f994ec57225f92d77a11a665cebabdedf22e48
Matin Rashidi <matinrashidi0118@gmail.com>
2022-02-10 16:19:41 +0330

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='w-full bg-teal-800 text-white font-semibold text-center py-2'>
+            <p>Developed by Matin</p>
+        </footer>
+    );
+};
+
+export default Footer;
